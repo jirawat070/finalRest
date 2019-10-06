461083eb8147d72b0928389f22ede52eac71dd35
jirawat070 <jirawat_pao@homail.co.th>
2019-10-06 09:07:56 +0700

    order getall point

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://ejhivrfxrvzsam:e6ea27848d6af9115aa2c68040e674502dbfb9d288840cac04cbda3e90607967@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d80ue59kfg9h7u?ssl=true');
 
 function getAllPoint(req, res) {
-    db.any('select * from public."Point"')
+    db.any('select * FROM public."Point" order by p_id')
         .then(function (data) {
             res.status(200)
                 .json({
@@ -58,23 +58,6 @@ function getRoomName(req, res) {
     })
 }
 
-function getPointName(req, res) {
-    console.log("req = "+req.params.name);
-    var x = req.params.name;
-    console.log('type'+typeof(x));
-    db.any('select * from public."Point" where p_name = ' + "'"+req.params.name+"'" ).then(function (data) {
-        res.status(200).json( 
-               data      
-        );
-    }).catch(function (error) {
-        console.log(error);
-        res.status(500).json({
-            status: 'failed',
-            data: data,
-            message: 'Failed To Retrieved ALL products'
-        });
-    })
-}
 
 function getMapInfo(req, res) {
     console.log("req = "+req.params.p_id);
